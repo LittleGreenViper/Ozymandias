45d16389c30621468cd6d634fec6f46fb0c1a269
Chris Marshall <chris@riftvalleysoftware.com>
2020-01-20 10:00:39 -0500

    Example 5. Initial Ozymandias Poem.

--- Ozymandias/main.swift
+++ Ozymandias/main.swift
@@ -54,3 +54,28 @@ textLineIndexes = textLineIndexes.map(addOffsetFunction)
 
 // We use the Array.map() function to add the offset, as it's the most effective Swift procedure.
 textLineIndexes = textLineIndexes.map { $0 + insertLineCount }
+
+func appendTextLines(_ inNewTextLineArray: [String], to: [String]) -> String {
+    return  (to + inNewTextLineArray).joined(separator: "\n")
+}
+
+let originalVersionPartOne = ["I met a traveller from an antique land",
+"Who said: Two vast and trunkless legs of stone",
+"Stand in the desert. Near them, on the sand,",
+"Half sunk, a shattered visage lies, whose frown,",
+"And wrinkled lip, and sneer of cold command,",
+"Tell that its sculptor well those passions read",
+"Which yet survive, stamped on these lifeless things,",
+"The hand that mocked them and the heart that fed:",
+"And on the pedestal these words appear:",
+"\"My name is Ozymandias, king of kings:",
+"Look on my works, ye Mighty, and despair!\""]
+
+let originalVersionPartTwo = [
+"Nothing beside remains. Round the decay",
+"Of that colossal wreck, boundless and bare",
+"The lone and level sands stretch far away."]
+
+var fullPoem = appendTextLines(originalVersionPartTwo, to: originalVersionPartOne)
+
+print(fullPoem)
